a794d76e4e5db41f8624025ff4725fdf4ec679a6
xtenrore <xtenrore@gmail.com>
2026-09-18 14:12:58 +0300

    Remove satellite map permanently and restore ADSB.fi button
    * Restore ADSB.fi button on aircraft alerts
    
    * Remove satellite-map rendering from Telegram alerts
    
    * Remove satellite-map runtime metadata
    
    * Remove satellite-map image dependency
    
    * Delete satellite-map renderer
    
    * Remove obsolete satellite-map tests
    
    * Test restored ADSB.fi alert button
    
    * Prevent satellite-map feature from returning

--- tests/test_photography_keyboards.py
+++ tests/test_photography_keyboards.py
@@ -7,3 +7,11 @@ def test_alert_keyboard_has_photo_and_feedback_actions():
     assert kb.inline_keyboard[0][0].callback_data == f"{PHOTO_CALLBACK_PREFIX}notif123"
     assert kb.inline_keyboard[1][0].callback_data.startswith("fb:like:")
     assert kb.inline_keyboard[1][1].callback_data.startswith("fb:dis:")
+
+
+def test_alert_keyboard_restores_adsbfi_button_when_icao_is_known():
+    kb = notification_actions_keyboard("notif123", "4BAA01")
+    assert len(kb.inline_keyboard) == 3
+    assert kb.inline_keyboard[0][0].text == "Open in ADSB.fi"
+    assert kb.inline_keyboard[0][0].url == "https://globe.adsb.fi/?icao=4baa01"
+    assert kb.inline_keyboard[1][0].callback_data == f"{PHOTO_CALLBACK_PREFIX}notif123"
